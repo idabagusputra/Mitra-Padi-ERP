23a96542d36314b8669544f98481b0774f01fd4b
Ida Bagus Putu Putra Manuaba <ibp.putra.m@gmail.com>
2024-11-18 17:51:02 +0700

    First Data

--- resources/views/receipt/thermal.blade.php
+++ resources/views/receipt/thermal.blade.php
@@ -149,7 +149,7 @@
             <tr class="calculation-row">
                 <td>Tanggal Nota </td>
                 <td>:</td>
-                <td>{{ $giling->created_at->addHours(7)->format('d/m/Y') }} ({{ $daftarGiling->created_at->addHours(7)->format('H:i:s') }})</td>
+                <td>{{ $daftarGiling->created_at->addHours(7)->format('d/m/Y') }} ({{ $daftarGiling->created_at->addHours(7)->format('H:i:s') }})</td>
             </tr>
         </table>
 
