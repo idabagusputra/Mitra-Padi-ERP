2e233e9e28dcade48e3bc13465f6a8b381e77ff6
Ida Bagus Putu Putra Manuaba <ibp.putra.m@gmail.com>
2024-11-23 22:09:20 +0700

    Update Kredit Trash

--- resources/views/layouts/navbars/auth/sidebar.blade.php
+++ resources/views/layouts/navbars/auth/sidebar.blade.php
@@ -100,7 +100,7 @@
                     <div class="icon icon-shape icon-sm shadow border-radius-md bg-white text-center me-2 d-flex align-items-center justify-content-center">
                         <i style="font-size: 1rem;" class="bi bi-trash3 ps-2 pe-2 text-center text-dark {{ (Request::is('kredit-trash') ? 'text-white' : 'text-dark') }} " aria-hidden="true"></i>
                     </div>
-                    <span class="nav-link-text ms-1">Kredit Trash</span>
+                    <span class="nav-link-text ms-1">Kredit Ryclebin</span>
                 </a>
             </li>
 
